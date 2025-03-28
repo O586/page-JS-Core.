a3b1ac4e2fe72dc216a93dc69e5fa057cc0ad2b0
0586 <git config --global user.email>
2025-03-28 17:36:05 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
 $(document).ready(function () {
+  var text1 = $("#s1").text();
 
-    var text1 = $('#s1').text();
+  var text2 = $(".s2").text();
 
-   
-    var text2 = $('.s2').text();
+  var text3 = $("#second i").text();
 
+  var text4 = $("q[title]").text();
 
-    var text3 = $('#second i').text(); 
+  var text5 = $('a[target="_blank"]').text();
 
-  
-    var text4 = $('q[title]').text();
+  var text6 = $("span.extra:eq(1)").text();
 
-   
-    var text5 = $('a[target="_blank"]').text();
+  var result =
+    text1 + " " + text2 + " " + text3 + " " + text4 + " " + text5 + " " + text6;
 
-   
-    var text6 = $('span.extra:eq(1)').text(); 
-
-  
-    var result = text1 + " " + text2 + " " + text3 + " " + text4 + " " + text5 + " " + text6;
-
- 
-    alert(result);
+  alert(result);
 });
